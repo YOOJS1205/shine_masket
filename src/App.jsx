2bb2593cb95b8316235f30fc1d19b951542a09c7
YOOJS1205 <junsang8921@gmail.com>
2022-06-30 00:15:11 +0900

    #5 - <Chore>: 파일 디렉토리 변경

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './styles/app.css';
 import './styles/reset.css';
-import Splash from './Splash/Splash';
+import Splash from './pages/Splash/Splash';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path='/' exact component={Splash} />
+      <Route path="/" exact component={Splash} />
     </BrowserRouter>
   );
 }

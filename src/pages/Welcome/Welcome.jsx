269e68d0fa4fa4697c9a6a0bc6e856a4bbe608f4
YOOJS1205 <junsang8921@gmail.com>
2022-07-05 00:45:28 +0900

    #24 - Feat: 로그인 기능 구현 (1)

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { Transition } from 'react-transition-group';
 import { motion } from 'framer-motion';
 import './transition.css';
 import Form from './Form';
@@ -7,16 +6,6 @@ import Top from './Top';
 
 export default function Welcome() {
   return (
-    // <Transition in={true} timeout={100} appear>
-    //   {
-    //     (status) => (
-    //       <div className={`fade-${status}`}>
-    //         <Top />
-    //         <Form />
-    //       </div>
-    //     )
-    //   }
-    // </Transition>
     <motion.div
       transition={{ duration: 0.5 }}
       initial={{ opacity: 0 }}

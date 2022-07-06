69f4195ca7d2496d8312b7d0709f8820defd9545
YOOJS1205 <junsang8921@gmail.com>
2022-07-06 10:22:37 +0900

    #35 - Refactor: 버튼 컴포넌트화, 로그인 버튼 함수명 변경

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Button({ buttonText, isEmpty, onClick }) {
+  return (
+    <ButtonComponent isEmpty={isEmpty} onClick={onClick}>
+      {buttonText}
+    </ButtonComponent>
+  )
+}
+
+const ButtonComponent = styled.button`
+  background-color: ${(props) =>
+    props.isEmpty ? 'var(--color-enabled)' : 'var(--color-enabled-dark)'};
+  color: #ffffff;
+  border-radius: 44px;
+  width: 100%;
+  border: none;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+  margin-top: 14px;
+  padding: 13px 0;
+`;

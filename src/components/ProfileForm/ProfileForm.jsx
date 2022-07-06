017952621accd03982dfe35f864be7211bcf2001
YOOJS1205 <junsang8921@gmail.com>
2022-07-06 23:54:27 +0900

    #47 - Feat: 프로필 설정 페이지 퍼블리싱

---
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import ImageButton from './ImageButton';
+import UserInfoInput from './UserInfoInput';
+import Button from '../Button/Button';
+
+export default function ProfileForm() {
+  return (
+    <FormContainer>
+      <ImageButton />
+      <UserInfoInput
+        TitleText="사용자 이름"
+        placeholder="2~10자 이내여야 합니다."
+      />
+      <UserInfoInput
+        TitleText="계정 ID"
+        placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+      />
+      <UserInfoInput
+        TitleText="소개"
+        placeholder="자신이 판매할 상품에 대해 소개해 주세요!"
+      />
+      <Button buttonText="샤인마스켓 시작하기" />
+    </FormContainer>
+  );
+}
+
+const FormContainer = styled.form`
+  padding: 0 34px;
+`;
--- src/pages/ProfileSetting/ProfileSetting.jsx
+++ src/pages/ProfileSetting/ProfileSetting.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import Title from '../../components/Title/Title';
+import ProfileForm from '../../components/ProfileForm/ProfileForm';
 
 export default function ProfileSetting() {
-  return <div>ProfileSetting</div>;
+  return (
+    <div>
+      <Title title="프로필 설정" />
+      <SubTitle>나중에 언제든지 변경할 수 있습니다.</SubTitle>
+      <ProfileForm />
+    </div>
+  );
 }
+
+const SubTitle = styled.h2`
+  font-size: 14px;
+  line-height: 14px;
+  text-align: center;
+  margin: 12px 0 30px;
+  color: #767676;
+`;

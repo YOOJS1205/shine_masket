4408c45a1f7788116292ba48efbd0236be2042c3
YOOJS1205 <junsang8921@gmail.com>
2022-07-06 19:53:30 +0900

    #42 - Fix: 로그인 관련 함수와 회원가입 관련 함수가 함께 작동하는 버그 수정

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from '../../components/Form/Form';
 import Title from '../../components/Title/Title';
 
 export default function Join() {
+  const [joinId, setJoinId] = useState('');
+  const [joinPassword, setJoinPassword] = useState('');
+
+  const getJoinInfo = (id, password) => {
+    useEffect(() => {
+      setJoinId(id);
+      setJoinPassword(password);
+    }, [id, password]);
+  };
   return (
     <div>
       <Title title="이메일로 회원가입" />
-      <Form buttonText="다음" />
+      <Form buttonText="다음" getJoinInfo={getJoinInfo} />
     </div>
   );
 }

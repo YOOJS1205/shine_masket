8e28985f55dda1f81907a11e1bb5028f75b5a314
YOOJS1205 <junsang8921@gmail.com>
2022-07-09 21:56:22 +0900

    #71 - Fix: 프로필 설정, 수정의 margin 수정

--- src/components/ProfileForm/InputTitle.jsx
+++ src/components/ProfileForm/InputTitle.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
 export default function InputTitle({ TitleText }) {
-  return (
-    <Title>{TitleText}</Title>
-  )
+  return <Title>{TitleText}</Title>;
 }
 
 const Title = styled.label`
